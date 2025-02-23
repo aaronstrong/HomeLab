ab4f8c5d7885c87ea44d640302e175974dd8fdbb
Aaron Strong <awstrong@gmail.com>
2025-02-23 14:58:52 -0600

    add pwoershell script to create a domain

--- terraform/proxmox/windows/scripts/create_domain.ps1
+++ terraform/proxmox/windows/scripts/create_domain.ps1
@@ -1,29 +1,22 @@
-# Define variables
-$onPremSiteName = "on-prem-location"
-$onPremSiteDescription = "Site1"
-$cloudSiteName = "cloud-us-central1"
-$cloudSiteDescription = "Site2"
-$subnet1 = "10.0.0.0/16"
-$subnet2 = "192.168.4.0/24"
-$replicationLinkName = "on-prem-cloud-us-central1"
-$replicationCost = 250
-$replicationFrequency = 15
-$transportProtocol = "IP"
+$domainName = "contoso.local"
+$netbiosName = "contoso"
+$safeModeAdminstratorPassword = ConvertTo-SecureString 'BestCloud1!' -AsPlainText -Force
+$logpath = "C:\log\log.txt"
+$domainMode = "Win2012R2"
+$forestMode = "Win2012R2"
 
-# Create on-premises site
-New-ADReplicationSite -Name $onPremSiteName -Description $onPremSiteDescription
+# Install Module
+Install-WindowsFeature -Name RSAT-AD-PowerShell
 
-# Create cloud site
-New-ADReplicationSite -Name $cloudSiteName -Description $cloudSiteDescription
-
-# Create replication site link
-New-ADReplicationSiteLink -Name $replicationLinkName -SitesIncluded $onPremSiteName,$cloudSiteName -Cost $replicationCost -ReplicationFrequencyInMinutes $replicationFrequency -InterSiteTransportProtocol $transportProtocol
-
-# Create subnets
-New-ADReplicationSubnet -Name $subnet1 -Site $cloudSiteName
-New-ADReplicationSubnet -Name $subnet2 -Site $onPremSiteName
-
-
-$localHostName = (Get-ComputerInfo).CsName
-
-Move-ADDirectoryServer -Identity $localHostName -Site "on-prem-location"
+# Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false  `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode $domainMode -ForestMode $forestMode `
+-DomainName $domainName `
+-DomainNetbiosName $netbiosname `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-SafeModeAdministratorPassword $safeModeAdminstratorPassword `
+-Force:$true
