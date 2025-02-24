da3744d602fe1db1838b7d3a23ff12b8751968cd
Aaron Strong <awstrong@gmail.com>
2025-02-24 17:11:12 -0600

    fix misspelling

---
+++ terraform/proxmox/windows/scripts/create-domain.ps1
@@ -0,0 +1,26 @@
+$domainName = "contoso.local"
+$netbiosName = "contoso"
+$safeModeAdminstratorPassword = ConvertTo-SecureString 'BestCloud1!' -AsPlainText -Force
+$logpath = "C:\log\log.txt"
+$domainMode = "Win2025"
+$forestMode = "Win2025"
+
+# Install Module
+Install-WindowsFeature -Name AD-Domain-Services, DNS -IncludeManagementTools
+Install-WindowsFeature -Name RSAT-AD-PowerShell -IncludeManagementTools
+
+# Import Active Directory Module into the PowerShell Session
+Import-Module ActiveDirectory
+
+# Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false  `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode $domainMode -ForestMode $forestMode `
+-DomainName $domainName `
+-DomainNetbiosName $netbiosname `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-SafeModeAdministratorPassword $safeModeAdminstratorPassword `
+-Force:$true
